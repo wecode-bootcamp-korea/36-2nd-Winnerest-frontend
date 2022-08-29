ac792567af5c8779d16331c6b040cee42564259d
kwon young jun <yonng0000@naver.com>
2022-08-29 17:45:02 +0900

    ADD: 초기세팅 완료

---
+++ src/pages/Login/Login.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Login = () =>{
+  return(
+    <div>Main</div>
+  )
+}
+
+export default Login;
---
+++ src/styles/theme.js
@@ -0,0 +1,6 @@
+const theme = {
+  basicColor: '#e60023',
+  middleGrey: '#767676',
+};
+
+export default theme;
