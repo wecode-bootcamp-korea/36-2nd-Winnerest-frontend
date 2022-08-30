5719f34b1937119a805bb4388e70cf0344482d5b
Joeunji0119 <95282989+Joeunji0119@users.noreply.github.com>
2022-08-30 19:05:51 +0900

    loginUI fin

--- src/Router.js
+++ src/Router.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Main from './pages/Main/Main';
 import Upload from './pages/Upload/Upload';
+import Login from './pages/Login/Login';
 
 const Router = () => {
   return (
@@ -9,6 +10,7 @@ const Router = () => {
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/upload" element={<Upload />} />
+        <Route path="/auth/signIn" element={<Login />} />
       </Routes>
     </BrowserRouter>
   );
