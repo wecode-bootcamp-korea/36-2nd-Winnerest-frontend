1f82cd5dafbbf9dd700a808f18f7c7b29e92c182
kwon young jun <yonng0000@naver.com>
2022-08-30 13:45:37 +0900

    ADD:button 스타일 기본세팅 적용

---
+++ src/components/Button/Button.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import styled, { css } from 'styled-components';
+
+const sizeStyles = css`
+  ${props =>
+    props.size === 'large' &&
+    css`
+      height: 50px;
+      font-size: 20px;
+    `}
+
+  ${props =>
+    props.size === 'medium' &&
+    css`
+      height: 35px;
+      font-size: 15px;
+    `}
+
+    ${props =>
+    props.size === 'small' &&
+    css`
+      height: 25px;
+      font-size: 10px;
+    `}
+`;
+
+const StyledButton = styled.button`
+  outline: none;
+  border: none;
+  border-radius: 25px;
+
+  ${sizeStyles}
+
+  color: white;
+  font-weight: bold;
+  cursor: pointer;
+
+  background-color: #e60023;
+
+  &:hover {
+    background-color: #eb3450;
+  }
+
+  &:active {
+    background-color: #e60023;
+  }
+
+  ${({ theme, color, hoverColor }) => {
+    const selected = theme.style[color];
+    const hover = theme.style[hoverColor];
+    return css`
+      background: ${selected};
+      &:hover {
+        background: ${hover};
+      }
+      &:active {
+        background: ${selected};
+      }
+    `;
+  }}
+`;
+
+function Button({ children, hoverColor, color, size, ...rest }) {
+  return (
+    <StyledButton color={color} hoverColor={hoverColor} size={size} {...rest}>
+      {children}
+    </StyledButton>
+  );
+}
+
+export default Button;
